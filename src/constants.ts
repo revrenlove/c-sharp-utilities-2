6982e6e6765a3ab739cb643cf21e0c4d4b802f14
revrenlove <58348549+revrenlove@users.noreply.github.com>
2024-12-08 19:37:41 -0600

    Added constants, config (w/ test) (#9)

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const TemplatePaths = {
+    fileScopedNamespace: `${__dirname}/templates/file-scoped-namespace.tmpl`,
+    namespaceEncapsulated: `${__dirname}/templates/namespace-encapsulated.tmpl`,
+};
+
+// TODO: #7 - JE - Change these!!!
+export const ExtensionPublisher = "revrenlove2";
+export const ExtensionName = "c-sharp-utilities2";
+export const NewGithubIssueUrl = "https://github.com/revrenlove/c-sharp-utilities-2/issues/new";
