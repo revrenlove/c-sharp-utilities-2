91eaecbd81a186414890c81a4855d07416c64ab3
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-01-07 20:17:33 -0600

    will it work?

--- .github/workflows/actions/pre-build-test-projects/index.mjs
+++ .github/workflows/actions/pre-build-test-projects/index.mjs
@@ -1,13 +1,22 @@
 import * as fs from "fs/promises";
 import * as path from "path";
+import { exec } from "child_process";
+import { promisify } from "util";
 
-// /home/runner/work/c-sharp-utilities-2/c-sharp-utilities-2/.github/workflows/actions/pre-build-test-projects
-// const testProjectsDirectoryPath = `${__dirname}/../../`;
+const execAsync = promisify(exec);
+
 const testProjectsDirectoryPath = `${path.resolve()}/out/test/assets/projects`;
-// const testProjectsDirectoryPath = `${path.resolve()}`;
 
 const filePaths = await fs.readdir(testProjectsDirectoryPath);
 
-filePaths.forEach(p => {
-    console.log(p);
+filePaths.forEach(async p => {
+    try {
+        const execResult = await execAsync("dotnet build", { "cwd": p });
+
+        return execResult.stdout;
+    }
+    catch (e) {
+        console.log("Errors are normal - some of the projects won't build, and that's by design.");
+        console.log(JSON.stringify(e));
+    }
 });
