882476834b61a84443c3b99d252631a65aba9c0a
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-01-07 18:40:58 -0600

    testing directory string

--- .github/workflows/actions/pre-build-test-projects/index.ts
+++ .github/workflows/actions/pre-build-test-projects/index.ts
@@ -1,2 +1,7 @@
+// /home/runner/work/c-sharp-utilities-2/c-sharp-utilities-2/.github/workflows/actions/pre-build-test-projects
 console.log(__dirname);
 
+// const testProjectsDirectoryPath = `${__dirname}/../../`;
+const testProjectsDirectoryPath = `${__dirname}/../../../../out/test/assets/projects`;
+
+console.log(testProjectsDirectoryPath);
