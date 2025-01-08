dd5b8ac2f97ee549e659c675885f29fb3d8f66d8
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-01-07 18:55:43 -0600

    more manual testing

--- .github/workflows/actions/pre-build-test-projects/index.ts
+++ .github/workflows/actions/pre-build-test-projects/index.ts
@@ -1,7 +1,11 @@
-// /home/runner/work/c-sharp-utilities-2/c-sharp-utilities-2/.github/workflows/actions/pre-build-test-projects
-console.log(__dirname);
+import * as fs from "fs/promises";
 
+// /home/runner/work/c-sharp-utilities-2/c-sharp-utilities-2/.github/workflows/actions/pre-build-test-projects
 // const testProjectsDirectoryPath = `${__dirname}/../../`;
 const testProjectsDirectoryPath = `${__dirname}/../../../../out/test/assets/projects`;
 
-console.log(testProjectsDirectoryPath);
+const filePaths = await fs.readdir(testProjectsDirectoryPath);
+
+filePaths.forEach(p => {
+    console.log(p);
+});
