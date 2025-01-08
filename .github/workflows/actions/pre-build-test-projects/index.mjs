d3e0804283668c6c209827277e832e49c8cd0073
revrenlove <58348549+revrenlove@users.noreply.github.com>
2025-01-07 19:38:34 -0600

    FUUUUU

--- .github/workflows/actions/pre-build-test-projects/index.mjs
+++ .github/workflows/actions/pre-build-test-projects/index.mjs
@@ -3,7 +3,8 @@ import * as path from "path";
 
 // /home/runner/work/c-sharp-utilities-2/c-sharp-utilities-2/.github/workflows/actions/pre-build-test-projects
 // const testProjectsDirectoryPath = `${__dirname}/../../`;
-const testProjectsDirectoryPath = `${path.resolve()}/out/test/assets/projects`;
+// const testProjectsDirectoryPath = `${path.resolve()}/out/test/assets/projects`;
+const testProjectsDirectoryPath = `${path.resolve()}`;
 
 const filePaths = await fs.readdir(testProjectsDirectoryPath);
 
